29b7b76ebffa9810497a2c6e9496bb056815f00d
daryllmamaril <dazzmamaril@gmail.com>
2021-07-06 20:32:26 +0800

    Added questions to app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const questions = [
+  new Question('Inside which HTML element do we put the JavaScript?', ['<javascript>', '<js>', '<script>', '<scripting>'], '<script>');
+  new Question('Which of the following is an advantage of using JavaScript?', ['Less server interaction', 'Immediate feedback to the visitors', 'Increased interactivity', 'All of the above.'], 'All of the above.');
+  new Question('Which of the following type of variable takes precedence over other if names are same?', ['Global Variable', 'Local Variable', 'Both of the Above', 'None of the Above'], 'Local Variable');
+  new Question('Which of the following function of Array object creates a new array with the results of calling a provided function on every element in this array?', ['push()', 'join()', 'pop()', 'map()'], 'map()');
+  new Question('Which of the following function of Array object applies a function simultaneously against two values of the array (from right-to-left) as to reduce it to a single value?', ['pop()', 'reduce()', 'reduceRight()', 'push()'], 'reduceRight()');
+];
